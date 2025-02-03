e04213726744f9413651c994d1e4956b44e85ae7
darkasfu <ashraful.islam.asfu@gmail.com>
2025-02-03 10:33:53 +0600

    Deploy vercel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Banner from "../Components/Banner";
 import Company from "../Components/Company";
-import Cta from "../Components/CTA";
+import Cta from "../Components/Cta";
 import Finance from "../Components/Finance";
 import OurPhilosophy from "../Components/OurPhilosophy";
 import TechnologyBuildForYou from "../Components/TechnologyBuildForYou";
